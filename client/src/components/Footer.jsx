bfc81f8e9322bc43e4121e0251ba93f2d138e5f8
Your Name <you@example.com>
2025-02-05 15:41:23 +0530

    home route

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom'
 import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
     <div className='container px-4 2xl:px-20 mx-auto flex items-center justify-between gap-4 py-3 mt-20'>
+      <Link to={'/'}>
+      
       <h1 className='font-bold text-2xl'> CareerNest</h1>
+      </Link>
       <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @arpit.dev | All right reserved.</p>
       <div className='flex gap-2.5'>
         <img width={38} src={assets.facebook_icon} alt="" />
